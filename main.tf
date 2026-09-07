60d12b8439288707b18f4a5454371f622529a0fe
Youssef Haridy <youssef.haridy@gmail.com>
2026-09-07 17:17:12 +0200

    fix variable

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_security_group" "blog" {
   name = "blog"
   description = "Allow http and hhttps in. Everything out"
 
-  vpc_id = data.aws_vpc.default.vpc_id
+  vpc_id = data.aws_vpc.default.id
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
