b789870a49df628b017667d821d14a8f1bea2e31
Youssef Haridy <youssef.haridy@gmail.com>
2026-09-07 18:16:05 +0200

    create new dev vpc

--- main.tf
+++ main.tf
@@ -13,26 +13,31 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
-data "aws_vpc" "default" {
-  default = true
+
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
-module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "6.0.0"
-  name = "blog_new"
-  
-  vpc_id = data.aws_vpc.default.id
-  
-  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
-  egress_rules       = ["all-all"]
-}
 
 resource "aws_security_group" "blog" {
   name = "blog"
   description = "Allow http and hhttps in. Everything out"
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.blog_vpc.vpc_id
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
@@ -68,7 +73,10 @@ resource "aws_security_group_rule" "blog_everything_out" {
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  vpc_security_group_ids = [aws_security_group.blog.id]
+
+  subnet_id = module.blog_vpc.public_subnets[0]
+
   tags = {
     Name = "HelloWorld"
   }
